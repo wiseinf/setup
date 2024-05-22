49edbc8c9cb550fedeccee1ac1931adcaa4fea98
JayLiu <liujian@wiseinf.com>
2024-05-22 14:23:16 +0800

    feat: add sql for user schema and its permissions.

---
+++ cluster-optimizer/sql/create_user_database.sql
@@ -0,0 +1,5 @@
+-- ----------------------------------------------------------------------------
+-- Schema user
+-- ----------------------------------------------------------------------------
+DROP SCHEMA IF EXISTS `user` ;
+CREATE SCHEMA IF NOT EXISTS `user` DEFAULT CHARACTER SET utf8mb4;
